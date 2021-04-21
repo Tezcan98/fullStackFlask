b41527b65f70dafa1db52b89df77ce3057b27a8d
Enes Tezcan <tezcan_enes@hotmail.com>
2021-04-21 13:06:25 +0300

    jq

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import models
 from flask_sqlalchemy import SQLAlchemy
 import listeler
 import os
+from flask import json
+
 
 app = Flask(__name__,static_url_path='', 
             static_folder='static')
@@ -23,30 +25,76 @@ db = SQLAlchemy(app)
 def home():
 	return render_template("index.html")
 
-@app.route("/create")
-def create():
-	return render_template("create.html")
+@app.route("/ilanlar")
+def ilanlar():  
 
- 
+	# sirk = models.sirket(sirket_ismi = "apple",Tarihce= "uzunyazi")
+	# db.session.add(sirk)
+	# db.session.commit()
+
+	# db.session.query(models.isler).delete()
+	# db.session.commit()
+	return render_template("ilanlar.html")
+
+
+@app.route('/ilanlistele', methods=['GET', 'POST'])
+def ilanlari_listele():
+	# if request.method == "POST": 
+	
+	# sirket = models.sirket_ilanlari.query.filter_by(yayinlayan_sirket= 0)
+	
+
+
+	ilanlar = models.isler.query.filter_by(id = 1).all()  #id verilecek
+	
+	ilanList = []
+	for st in ilanlar:
+		ilanList.append(st.liste())
+	
+	response = app.response_class(
+            response=json.dumps(
+				{	
+					"html": render_template("ilanlariListele.html") , 
+					"status":"success",
+					'ilanlar' : ilanList,
+					"code":0
+
+				}),
+            status=200,
+            mimetype='application/json'
+		)
+	return response 
+
 @app.route('/ilanolustur', methods=['GET', 'POST'])
 def ilanolustur(): 
 	if request.method == "POST":
 		yeni_is = models.isler(
 			#### ajax ile istek oluşturulacak
-			Baslik = request.form["ilanbasligi"],
-			is_turu =  listeler.is_turu[int(request.form["isturu"])],
-		    istenen_tecrube =  listeler.tecrube[int(request.form["tecrube"])],
-		    il   =  request.form["il"],
-		    ilce =  request.form["ilce"],
-		    gorunen_aciklama =  request.form["gorunen"],
-		    maas_bilgisi =  request.form["maas"],
-		    detayli_aciklama = request.form["detayli"]
+			Baslik = request.form.get('Baslik',''),
+			is_turu =request.form.get('is_turu',''),
+		    istenen_tecrube =  request.form.get('istenen_tecrube','')  ,
+		    il   =  request.form.get('il',''),
+		    ilce =  request.form.get('ilce','') ,
+		    gorunen_aciklama =  request.form.get('gorunen_aciklama','')  ,
+		    maas_bilgisi =  request.form.get('maas_bilgisi','')  ,
+		    detayli_aciklama = request.form.get('detayli_aciklama','')  
 			)
-			
-		
 		db.session.add(yeni_is) 
 		db.session.commit()
-		return 200
+		sirket_ilanlari = models.sirket_ilanlari( 
+			yayinlanan_is = yeni_is.id,
+			yayinlayan_sirket = models.sirket.query.get(0).id # id verilecek
+		)
+		
+		db.session.add(sirket_ilanlari)
+		db.session.commit()
+
+		response = app.response_class(
+            response=json.dumps({"status":"success","code":0}),
+            status=200,
+            mimetype='application/json'
+		)
+		return response
 
 		# return str(models.isler.query.all()[0])
 		# return Listeler.tecrube[int(request.form["tecrube"])]
--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from flask_appbuilder import Model
 from app import db
 from sqlalchemy import Column, Integer, String, ForeignKey, Date
 # before pip install flask_appbuilder, run this command ->>  pip install setuptools --upgrade
-
+from datetime import datetime 
 from flask_appbuilder.models.mixins import ImageColumn
 
 
@@ -16,11 +16,8 @@ class isler(db.Model):
     ilce = Column(String(32), nullable=True)
     gorunen_aciklama = Column(String(256), nullable=False)
     maas_bilgisi = Column(String(32), nullable=True)
-    detayli_aciklama = Column(String(1024), nullable=False)
+    detayli_aciklama = Column(String(1024), nullable=False) 
  
-    # kullanici = db.relationship("kullanici", backref="kullanici") 
-    # sirket = db.relationship("sirket", backref="sirket")
-
     
     def __init__(self, Baslik, is_turu, istenen_tecrube, il, ilce, gorunen_aciklama, maas_bilgisi, detayli_aciklama): 
         self.Baslik = Baslik
@@ -30,12 +27,13 @@ class isler(db.Model):
         self.ilce =  ilce
         self.gorunen_aciklama =  gorunen_aciklama
         self.maas_bilgisi = maas_bilgisi
-        self.detayli_aciklama = detayli_aciklama
+        self.detayli_aciklama = detayli_aciklama 
 
+    # def listele(self, filtre, siralama, sayfa): # sayfada 12 tane olucak şekilde
+    #   return 1
 
-    def listele(self, filtre, siralama, sayfa): # sayfada 12 tane olucak şekilde
-      return 1
-
+    def liste(self):
+        return [self.Baslik, self.detayli_aciklama]
     def __str__(self):
         return "Baslik : "+self.Baslik + "Detay : " + self.detayli_aciklama
 
@@ -58,17 +56,38 @@ class kullanici(db.Model):
     # cv fk
     cv = Column(Integer, ForeignKey('cvs.id'))  
     photo = Column(ImageColumn(size=(300, 300, True), thumbnail_size=(30, 30, True)))
-    #basvurular fk
-    basvurulan_isler = Column(Integer, ForeignKey('isler.id'))  
+    
 
 class sirket(db.Model):
     __tablename__ = 'sirket'
     id = Column(Integer, primary_key=True, autoincrement=True)
     sirket_ismi = Column(String(75), unique = True, nullable=False)
-    Tarihce = Column(String(512), nullable=True)
-    #ilan fk 
-    ilanlar = Column(Integer, ForeignKey('isler.id')) 
+    Tarihce = Column(String(512), nullable=True) 
 
+    def __init__(self,sirket_ismi, Tarihce): 
+        self.sirket_ismi = sirket_ismi
+        self.Tarihce = Tarihce 
 
+class is_basvurulari(db.Model):
+    __tablename__ = 'is_basvurulari'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    #basvurular fk
+    bas_is = Column(Integer, ForeignKey('isler.id'))  
+    bas_kisi = Column(Integer, ForeignKey('kullanici.id'))  
+    tarih = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class sirket_ilanlari(db.Model):
+    __tablename__ = 'sirket_ilanlari'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    yayinlanan_is = Column(Integer, ForeignKey('isler.id'))  
+    yayinlayan_sirket = Column(Integer, ForeignKey('sirket.id'))  
+    tarih = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    
+    def __init__(self, yayinlanan_is, yayinlayan_sirket,):
+        self.yayinlanan_is = yayinlanan_is
+        self.yayinlayan_sirket = yayinlayan_sirket 
+        self.tarih = datetime.now()
 
 db.create_all()
+
+ 
